3c01f3e464aabec0bea9e4c7f4eb83009c648162
flywaiting <flywaiting@hotmail.com>
2023-05-31 22:13:18 +0800

    edit move

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -1,5 +1,42 @@
 return {
 	{
+		'kevinhwang91/nvim-ufo',
+		keys = { 'za' },
+		dependencies = {
+			'kevinhwang91/promise-async',
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require "statuscol.builtin"
+					require("statuscol").setup {
+						relculright = true,
+						segments = {
+							{ text = { builtin.foldfunc },      click = "v:lua.ScFa" },
+							{ text = { "%s" },                  click = "v:lua.ScSa" },
+							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+						},
+					}
+				end,
+			},
+		},
+		config = function()
+			vim.opt.foldcolumn = '1'
+			vim.opt.foldlevel = 99
+			vim.opt.foldlevelstart = 99
+			vim.opt.foldenable = true
+
+			vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+			vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+			-- require('ufo').setup({
+			-- 	provider_selector = function (_, _)
+			-- 		return { 'lsp', 'treesitter', 'indent' }
+			-- 	end
+			-- })
+			require('ufo').setup()
+		end
+	},
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "BufReadPre",
 		config = function()
@@ -40,10 +77,15 @@ return {
 	},
 	{
 		"numToStr/Comment.nvim",
-		event = "BufRead",
-		-- keys = { "gc", "gcc", "gbc" },
+		-- event = "BufRead",
+		keys = { "gc", "gcc", "gbc" },
 		config = function()
 			require("Comment").setup()
 		end,
 	},
+	{
+		'fedepujol/move.nvim',
+		cmd = { 'MoveLine', 'MoveWord', 'MoveBlock'},
+
+	}
 }
