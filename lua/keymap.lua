139d985977bb841d376c46ef6d47c55db2c93e27
flywaiting <flywaiting@hotmail.com>
2023-11-28 18:01:01 +0800

    .

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,14 +4,23 @@ local map = vim.keymap.set
 map("n", "<c-b>", ":NvimTreeToggle<cr>", opt)
 map("i", "<c-b>", "<esc>:NvimTreeToggle<cr>", opt)
 
--- window jump and split
-map("n", "<c-l>", "<c-w>l", opt)
+map("n", "<c-s>", ":w<cr>", opt)
+map("i", "<c-s>", "<esc>:w<cr>", opt)
+
+map("n", "sv", ":vsp<cr>", opt)
+map("n", "sh", ":sp<cr>", opt)
+map("n", "sc", "<c-w>c", opt)	-- close current 
+map("n", "so", "<c-w>o", opt)	-- close other
+-- window jump
 map("n", "<c-down>", "<c-w>j", opt)
 map("n", "<c-up>", "<c-w>k", opt)
 map("n", "<c-left>", "<c-w>h", opt)
 map("n", "<c-right>", "<c-w>l", opt)
-map("n", "<leader>v", "<c-w>v", opt)
-map("n", "<leader>s", "<c-w>s", opt)
+
+map("n", "q", ":q<cr>", opt)
+map("n", "qq", ":q!<cr>", opt)
+map("n", "Q", ":qa!<cr>", opt)
+
 -- logic line and view line
 map("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
 map("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
