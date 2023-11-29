d5191dbb9ae2e303f395f789166fa2f7f4f65025
flywaiting <flywaiting@hotmail.com>
2023-11-29 23:58:18 +0800

    tree sitter, base config

---
+++ lua/plugin/list/nvim-treesitter.lua
@@ -0,0 +1,39 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "BufEnter",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })()
+		end,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+				},
+				sync_install = false,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+					-- disable for large buf
+					-- disable = function(lang, buf)
+					-- end,
+				},
+				indent = {
+					enable = true,
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						-- init_selection = "<cr>",
+						init_selection = "gnn",
+						node_incremental = "<cr>",
+						node_decremental = "<bs>",
+						scope_incremental = "<tab>",
+					},
+				},
+			})
+		end,
+	},
+}
