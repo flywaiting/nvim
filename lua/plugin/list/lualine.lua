4c00222e5a09a1460b746534052073008674df21
flywaiting <flywaiting@hotmail.com>
2023-11-29 18:01:31 +0800

    lualine

---
+++ lua/plugin/list/lualine.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "UIEnter",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			-- "f-person/git-blame.nvim",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					disabled_filetypes = {
+						"NvimTree",
+					},
+				},
+				-- sections = {
+				-- 	lualine_c = {
+				-- 		{
+				-- 			require("gitblame").get_current_blame_text,
+				-- 			cond = require("gitblame").is_blame_text_available
+				-- 		},
+				-- 	},
+				-- 	lualine_x = {
+				-- 		{
+				-- 			require("lazy.status").updates,
+				-- 			cond = require("lazy.status").has_updates,
+				-- 			color = { fg = "#ff9e64" },
+				-- 		},
+				-- 	},
+				-- },
+			})
+		end,
+	}
+}
