4ed517242740131a44115e8b5e19abcba18800ca
flywaiting <flywaiting@hotmail.com>
2023-12-05 14:13:20 +0800

    lsp for lua

--- lua/plugin/list/lsp/configs/lua_ls.lua
+++ lua/plugin/list/lsp/configs/lua_ls.lua
@@ -1,21 +1,35 @@
 return {
-	Lua = {
-		runtime = {
-			version = "LuaJIT",
-		},
-		workspace = {
-			checkThirdParty = false,
-		},
-		hint = {
-			enable = true,
-			arrayIndex = "Enable",
-			setType = true,
-		},
-		diagnostics = {
-			-- get the language server to recognize the 'vim' global
-			globals = {
-				"vim",
-			}
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,								-- install dir/share/nvim/runtime
+					-- vim.fn.expand("$VIMRUNTIME/lua"),
+					-- vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+					vim.fn.stdpath("config") .. "lua",				-- self config/lua
+				}
+			},
+			hint = {
+				enable = true,
+				arrayIndex = "Enable",
+				setType = true,
+			},
+			diagnostics = {
+				enable = true,
+				-- get the language server to recognize the 'vim' global
+				globals = {
+					"vim",
+					"describe",
+					"it",
+					"before_each",
+					"after_each",
+				},
+			},
 		}
 	}
 }
